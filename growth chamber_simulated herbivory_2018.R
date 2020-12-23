0293856f7a0a8cafb48f66984aa90f5f4dcedf88
klapierre <lapierre.kimberly@gmail.com>
2020-12-23 18:02:53 -0500

    Growth chamber model updates to nlme and repeated measures.

--- growth chamber_simulated herbivory_2018.R
+++ growth chamber_simulated herbivory_2018.R
@@ -1,6 +1,20 @@
+################################################################################
+##  growth chamber_simulated herbivory_2018.R: Examining the effects of rhizobial diversity on soybean resilience following simulated herbivory.
+##
+##  Author: Kimberly Komatsu
+##  Date created: July 29, 2018
+################################################################################
+
+library(nlme)
+library(lsmeans)
+library(car)
+# library(ggeffects)
+# library(sjPlot)
+# library(grid)
+library(performance)
+library(piecewiseSEM)
 library(tidyverse)
-library(lme4)
-library(lmerTest)
+library(ggpubr)
 
 setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\SERC\\interns\\2018\\2018_REU_Esch\\final data_komatsu approved')
 
@@ -9,7 +23,7 @@ theme_update(axis.title.x=element_text(size=20, vjust=-0.35), axis.text.x=elemen
              axis.title.y=element_text(size=20, angle=90, vjust=0.5), axis.text.y=element_text(size=16),
              plot.title = element_text(size=24, vjust=2),
              panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
-             legend.title=element_blank(), legend.text=element_text(size=20))
+             legend.title=element_text(size=20), legend.text=element_text(size=20))
 
 ###bar graph summary statistics function
 #barGraphStats(data=, variable="", byFactorNames=c(""))
@@ -41,10 +55,24 @@ pots <- read.csv('Inside_datafinal.csv')%>%
   unique()
 trt <- read.csv('Inside_treatments.csv')%>%
   mutate(treatment=number)%>%
-  select(-number)%>%
+  select(-number)%>%mutate(strains_code=ifelse(diversity_description=='1', 'A',
+                                               ifelse(diversity_description=='2', 'B',
+                                                      ifelse(diversity_description=='3', 'C',
+                                                             ifelse(diversity_description=='4', 'D',
+                                                                    ifelse(diversity_description=='1,2', 'AB',
+                                                                           ifelse(diversity_description=='1,3', 'AC',
+                                                                                  ifelse(diversity_description=='1,4', 'AD',
+                                                                                         ifelse(diversity_description=='2,3', 'BC',
+                                                                                                ifelse(diversity_description=='2,4', 'BD',
+                                                                                                       ifelse(diversity_description=='3,4', 'CD',
+                                                                                                              ifelse(diversity_description=='1,2,3', 'ABC',
+                                                                                                                     ifelse(diversity_description=='1,2,4', 'ABD',
+                                                                                                                            ifelse(diversity_description=='1,3,4', 'ACD',
+                                                                                                                                   ifelse(diversity_description=='2,3,4', 'BCD', 'control')))))))))))))))%>%
   left_join(pots)
 growth <- read.csv('Inside_datafinal.csv')%>%
-  left_join(trt)
+  left_join(trt)%>%
+  mutate(date=as.Date(date, format='%m/%d/%y', origin='1899-12-30'))
 nodules <- read.csv('Inside_nodule number.csv')%>%
   left_join(trt)
 biomass <- read.csv('Inside_biomass.csv')%>%
@@ -52,106 +80,101 @@ biomass <- read.csv('Inside_biomass.csv')%>%
   mutate(total_biomass=shoot+root)
 
 ###repeated measures ANOVA for height
-summary(heightRManova <- lmer(height ~ diversity_treatment*herbivory + (1+pot|date), data=subset(growth, diversity_treatment!=0)))
+summary(heightModel <- lme(log10(height)~as.factor(diversity_treatment)*as.factor(herbivory)*date,
+                           data=subset(growth, diversity_treatment!=0 & num_leaves!=0), 
+                           random=~1|block,
+                           correlation=corAR1(form=~date|block/pot),
+                           control=lmeControl(returnObject=T)))
+check_model(heightModel)
+anova.lme(heightModel, type='marginal') #significant effect of time
 
-ggplot(data=barGraphStats(data=subset(growth, date=='7/23/18' & diversity_treatment!=0), variable="height", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
+heightPlot <- ggplot(data=barGraphStats(data=subset(growth, diversity_treatment!=0 & num_leaves!=0), variable="height", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Height (cm)') +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
+  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'),
+                    name='Simulated\nHerbivory')
 
 
 ###repeated measures ANOVA for leaf number
-summary(leavesRManova <- lmer(num_leaves ~ diversity_treatment*herbivory + (1+pot|date), data=subset(growth, diversity_treatment!=0)))
+summary(leavesRManova <- lme(log10(num_leaves)~as.factor(diversity_treatment)*as.factor(herbivory)*date,
+                           data=subset(growth, diversity_treatment!=0 & num_leaves!=0), 
+                           random=~1|block,
+                           correlation=corAR1(form=~date|block/pot),
+                           control=lmeControl(returnObject=T)))
+check_model(leavesRManova)
+anova.lme(leavesRManova, type='marginal') #significant effect of time
 
-ggplot(data=barGraphStats(data=subset(growth, date=='7/23/18' & diversity_treatment!=0), variable="num_leaves", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
+ggplot(data=barGraphStats(data=subset(growth, diversity_treatment!=0 & num_leaves!=0), variable="num_leaves", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Leaf Number') +
   scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
 
 
 ###ANOVA for nodule number
-summary(nodulesGLM <- aov(nodules ~ diversity_treatment*herbivory, data=subset(nodules, diversity_treatment!=0)))
+summary(nodulesANOVA <- lme(sqrt(nodules)~as.factor(diversity_treatment)*as.factor(herbivory),
+                             data=subset(nodules, diversity_treatment!=0), 
+                             random=~1|block))
+anova.lme(nodulesANOVA, type='marginal') #no effect
 
-ggplot(data=barGraphStats(data=subset(nodules, diversity_treatment!=0), variable="nodules", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
+nodulePlot <- ggplot(data=barGraphStats(data=subset(nodules, diversity_treatment!=0), variable="nodules", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Nodule Number') +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
+  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'),
+                    name='Simulated\nHerbivory')
 
 
 ###ANOVA for biomass
-summary(shootGLM <- aov(shoot ~ diversity_treatment*herbivory, data=subset(biomass, diversity_treatment!=0)))
+summary(shootANOVA <- lme(shoot~as.factor(diversity_treatment)*as.factor(herbivory),
+                            data=subset(biomass, diversity_treatment!=0), 
+                            random=~1|block))
+anova.lme(shootANOVA, type='marginal') #no effect
 
-ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="shoot", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
+shootPlot <- ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="shoot", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Shoot Biomass (g)') +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
+  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'),
+                    name='Simulated\nHerbivory')
 
 
-summary(rootGLM <- aov(root ~ diversity_treatment*herbivory, data=subset(biomass, diversity_treatment!=0)))
+summary(rootANOVA <- lme(root~as.factor(diversity_treatment)*as.factor(herbivory),
+                          data=subset(biomass, diversity_treatment!=0), 
+                          random=~1|block))
+anova.lme(rootANOVA, type='marginal') #no effect
 
-ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="root", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
+rootPlot <- ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="root", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Root Biomass (g)') +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
+  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'),
+                    name='Simulated\nHerbivory')
 
-summary(totbioGLM <- aov(total_biomass ~ diversity_treatment*herbivory, data=subset(biomass, diversity_treatment!=0)))
+
+
+summary(totbioANOVA <- lme(total_biomass~as.factor(diversity_treatment)*as.factor(herbivory),
+                         data=subset(biomass, diversity_treatment!=0), 
+                         random=~1|block))
+anova.lme(totbioANOVA, type='marginal') #no effect
 
 ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="total_biomass", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, fill=herbivory)) +
   geom_bar(stat='identity', position=position_dodge()) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
+  xlab('Rhizobial Diversity') +
   ylab('Total Biomass (g)') +
   scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
-
-ggplot(data=barGraphStats(data=subset(biomass, diversity_treatment!=0), variable="total_biomass", byFactorNames=c("diversity_treatment", "herbivory")), aes(x=as.factor(diversity_treatment), y=mean, color=herbivory)) +
-  geom_point(size=5, position=position_dodge(0.9)) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Number') +
-  ylab('Total Biomass (g)') +
-  scale_color_manual(values=c('dark orange', 'dark green', 'dark blue'))
 #export at 665 x 610
 
-###monocultures only
-###repeated measures ANOVA for height
-summary(heightRManova <- lmer(height ~ diversity_description*herbivory + (1+pot|date), data=subset(growth, diversity_treatment==1)))
 
-ggplot(data=barGraphStats(data=subset(growth, date=='7/23/18' & diversity_treatment==1), variable="height", byFactorNames=c("diversity_description", "herbivory")), aes(x=as.factor(diversity_description), y=mean, fill=herbivory)) +
-  geom_bar(stat='identity', position=position_dodge()) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Identity') +
-  ylab('Height (cm)') +
-  scale_x_discrete(labels=c('110', '76', '136', '138')) +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
-#export at 800x800
 
-###repeated measures ANOVA for leaf number
-summary(leavesRManova <- lmer(num_leaves ~ diversity_description*herbivory + (1+pot|date), data=subset(growth, diversity_treatment==1)))
-
-ggplot(data=barGraphStats(data=subset(growth, date=='7/23/18' & diversity_treatment==1), variable="num_leaves", byFactorNames=c("diversity_description", "herbivory")), aes(x=as.factor(diversity_description), y=mean, fill=herbivory)) +
-  geom_bar(stat='identity', position=position_dodge()) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Identity') +
-  ylab('Leaf Number') +
-  scale_x_discrete(labels=c('110', '76', '136', '138')) +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
-#export at 800x800
-
-###ANOVA for nodule number
-summary(nodulesGLM <- aov(nodules ~ diversity_description*herbivory, data=subset(nodules, diversity_treatment!=0)))
-
-ggplot(data=barGraphStats(data=subset(nodules, diversity_treatment==1), variable="nodules", byFactorNames=c("diversity_description", "herbivory")), aes(x=as.factor(diversity_description), y=mean, fill=herbivory)) +
-  geom_bar(stat='identity', position=position_dodge()) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
-  xlab('Rhizobial Strain Identity') +
-  ylab('Nodule Number') +
-  scale_fill_manual(values=c('dark orange', 'dark green', 'dark blue'))
+#resilience figure
+ggarrange(heightPlot, shootPlot, rootPlot, nodulePlot,
+          ncol = 2, nrow = 2)
+#export at 800x1800
